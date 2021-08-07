7296e1872dc4d30d984627ca3907b8cbb5b77a30
Alex-Roland <47874780+Alex-Roland@users.noreply.github.com>
2021-08-07 10:16:52 -0500

    Add files via upload

---
+++ CHANGELOG.md
@@ -0,0 +1,37 @@
+Unreleased - planned
+--------------------
+Organize output so the multithreading doesn't break up the results
+Ability to search for specific CIDRs, example: -i 172.17.10.4 -c /24, this will search both the specific IP and /24. You can also just search the CIDR.
+    Might do this to where either the entire CIDR range is searched or just the network ID
+    *Add ipaddress module for this*
+
+
+Version 2.1.3 - 2021-08-05
+--------------------------
+Added: Check for logs folder in log builder
+
+
+Version 2.1.2 - 2021-07-28
+--------------------------
+Fixed: Core router discovery wasn't implemented
+Added: Check for and create daily runtime folders to better organize the log outputs
+
+
+Version 2.1.1 - 2021-07-26
+--------------------------
+Added: -V option to print version information
+
+
+Version 2.1 - 2021-07-22
+------------------------
+Added: XMC API call for dynamic inventory
+
+
+Version 2.0 - 2021-02-01
+------------------------
+Added: Multi-threading capabilities
+
+
+Version 1.0 - 2020-06-12
+------------------------
+Initial release
---
+++ act.py
@@ -0,0 +1,294 @@
+#!/usr/bin/env python3
+
+# MIT License
+#
+# Copyright (c) 2021 Alex Roland
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import argparse
+import requests
+import json
+import getpass
+import re
+import os
+from threading import Thread, currentThread, Lock
+from queue import Queue
+from datetime import datetime
+from netmiko import ConnectHandler
+from urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(category = InsecureRequestWarning) # This will disable certificate warning output when running the script
+
+parser = argparse.ArgumentParser(description = 'Checks ACLs for an IP match')
+parser.add_argument('-i', help = 'ip to check', action='append')
+parser.add_argument('-s', help = 'XMC hostname / IP (default: xmc.pnw.edu)', default = 'xmc.pnw.edu')
+parser.add_argument('-u', help = 'username for SSH authentication (default: admin)', default = 'admin')
+parser.add_argument('-k', help = 'prompt for SSH password (default: false)', default = False, action = 'store_true')
+parser.add_argument('-t', help = 'number of threads to run (default: 20)', type=int, default = 20)
+parser.add_argument('-w', help = 'filename to write report to (default: [time]-[ip]-report.log)', default = 'report.log')
+parser.add_argument('-n', help = 'do not validate HTTPS certificates (default: false)', default = False, action = 'store_true')
+parser.add_argument('-V', help = 'display version information', default = False, action = 'store_true')
+args = parser.parse_args()
+
+NAME = 'ACL Check Tool'
+VERSION = '2.1.3'
+DATE = '8/5/2021'
+
+if args.V == True: # Script version information
+    print('|' + '-'*30 + '|')
+    print('|' + ' '*8 + NAME + ' '*8 + '|')
+    print('|' + '-'*30 + '|')
+    print('|' + ' '*4 + VERSION + ' '*5 + '|' + ' '*3 + DATE + ' '*3 + '|')
+    print('|' + '-'*30 + '|')
+    exit(0)
+
+if args.i is None: # Checks if user provided an IP to check against
+    print('Please provide at least one IP to check')
+    exit(0)
+
+current_date = datetime.now().strftime('%Y-%m-%d')
+
+NUM_THREADS = args.t # Used to set the maximum amount of parallel connections
+PRINT_LOCK = Lock() # This will prevent the output from being sent out of order
+
+api_url = 'https://' + args.s + ':8443/nbi/graphql' # URL of the XMC server, pulled from -s flag in argparse
+
+http_params = {
+    'query': 'query { network { devices { ip nickName nosIdName } } }' # XMC API query to run, more info at https://{xmc_server}:8443/nbi/graphiql/index.html then click on Docs in the top right
+}
+
+NET_DEVICE = { # Basic device dictionary, this is copied in the main function and values are then filled in
+    'device_type': '',
+    'ip': '',
+    'username': args.u,
+    'password': '',
+    'conn_timeout': 10,
+}
+
+def log_folder():
+    if os.path.isdir('logs'):
+        return
+    else:
+        os.mkdir('logs')
+
+def daily_runtime_folder():
+    if os.path.isdir('logs/' + current_date):
+        return
+    else:
+        os.mkdir('logs/' + current_date)
+
+def log_file(file): # Function to output to the screen and log the output to a file
+    def write_log(msg):
+        with PRINT_LOCK:
+            with open('logs/' + current_date + '/' + file, 'a') as f:
+                print(msg, file=f)
+            return msg # Returns the user message so the message can be assigned to a variable for further data processing
+    return write_log # Returns nested function
+
+def log_sum(file): # Function to output to the screen and log the output to a file
+    def write_log(msg):
+        with PRINT_LOCK:
+            with open('logs/' + current_date + '/' + file, 'a') as f:
+                print(msg, file=f)
+                print(msg)
+            return msg # Returns the user message so the message can be assigned to a variable for further data processing
+    return write_log # Returns nested function
+
+def grab_routers(nickName):
+    if 'r' in nickName.split('-')[-1] or 'c' in nickName.split('-')[-1]: # Grab r(outer) or c(ore), example: nw-hmd-pwrs-226-x690-c1 will match due to the c in c1
+        is_router = True
+    else:
+        is_router = False
+    return is_router
+
+def discover_vendor(nickName, nosIdName): # Function to get the network OS so commands are run correctly
+    if '-' in nickName and 'pnw.edu' not in nickName and 'purdue.edu' not in nickName:
+        letter = nickName.split('-')[-2][0] # Grab letter for NOS detection, example: for nw-hmd-unsv-202-x590-r2 this will grab 'x'
+    else:
+        logger(nickName + ' does not conform to naming conventions')
+        nos = 'null'
+        return nos
+    if letter == 'x':
+        nos = 'extreme_exos'
+    elif letter == 'e':
+        nos = 'enterasys'
+    elif letter == 'c':
+        if 'Nexus' in nosIdName:
+            nos = 'cisco_nxos'
+        elif 'Cisco' in nosIdName:
+            nos = 'cisco_ios'
+    elif letter == 'b':
+        nos = 'ruckus_fastiron'
+    else:
+        nos = 'null'
+    return nos
+
+def check_ip(output, ip_list, ip, hostname):
+    config_list = output.splitlines() # Read each config line into a new list
+    ipcounter = 0
+    for item in config_list:
+        for addr in ip_list:
+            if re.search(r'\b{}\b'.format(ip_list[ipcounter]), item): # Use regex word boundaries to search for an exact IP match anywhere in the configuration
+                out_sum(f'{ip}({hostname}) --> {item}')
+                logger(f'{ip}({hostname}) --> {item}')
+            ipcounter += 1
+        ipcounter = 0
+
+def run_threads(total_devices, mt_function, dq, dd, ip_list, **kwargs): # dq is the device queue and dd is the device details
+    total_threads = min(NUM_THREADS, total_devices) # If the amount of loaded devices is less than the max thread count, then limit the amount of threads to device count
+    for i in range(total_threads):
+        thread_name = f'Thread-{i}'
+        worker = Thread(name=thread_name, target=mt_function, args=(dq, dd, ip_list, kwargs))
+        worker.start()
+    worker.join() # Waits for all threads to stop before ending script
+
+def send_commands(dq, dd, ip_list, kwargs): # Function to perform tasks on each line in the devices file
+    while True:
+        thread_name = currentThread().getName()
+        ipcounter = 0
+
+        if dq.empty(): # Check if device queue is empty
+            logger(f'{thread_name}: Closing since there are no jobs left in the queue.')
+            return
+
+        nc_params = dq.get() # Create variable to fill with device queue information per iteration
+        device_detail = dd.get() # Pull original device line, used for more specific device information so we can reference different points of data
+        ip = nc_params['ip'] # Pull the IP for output formatting
+        hostname = device_detail['nickName'] # The nickname is also the hostname of the device
+
+        logger(f'{thread_name} {ip}: Connecting...')
+        try:
+            nc = ConnectHandler(**nc_params) # Establishes SSH connectivity
+            logger(f'{thread_name} {ip}: Connected!')
+            logger(f'{thread_name} {ip}: Sending commands...')
+            try:
+                if nc_params['device_type'] == 'extreme_exos':
+                    policy = re.findall(r"[a-zA-Z0-9_\\\-\.\(\):]+\.pol", nc.send_command('ls'))
+                    logger(f'{thread_name} {ip} output:\n{policy}')
+                    if policy != []:
+                        for i in range(len(policy)):
+                            policyfile = policy[i].strip('.pol')
+                            for addr in ip_list:
+                                ipcheck = nc.send_command_timing(f'show policy {policyfile} | i {ip_list[ipcounter]}')
+                                if re.search(rf'\b{ip_list[ipcounter]}\b', ipcheck) and not "Unrecognized" in ipcheck:
+                                    output = policyfile + " has line " + ipcheck
+                                    logger(f'{thread_name} {ip} output:\n' + output)
+                                    out_sum(f'{ip}({hostname}) --> {output}')
+                                ipcounter += 1
+                            ipcounter = 0
+                    else:
+                        logger('No policy found')
+                elif nc_params['device_type'] == 'enterasys':
+                    output = nc.send_command('show config')
+                    check_ip(output, ip_list, ip, hostname)
+                elif nc_params['device_type'] == 'cisco_ios':
+                    output = nc.send_command('show run')
+                    check_ip(output, ip_list, ip, hostname)
+                elif nc_params['device_type'] == 'cisco_nxos':
+                    output = nc.send_command('show run')
+                    check_ip(output, ip_list, ip, hostname)
+                elif nc_params['device_type'] == 'ruckus_fastiron':
+                    output = nc.send_command('show run')
+                    check_ip(output, ip_list, ip, hostname)
+                else:
+                    raise
+            except BaseException as e:
+                log_failed(f'{thread_name} {ip}: {e}')
+                nc.disconnect() # Gracefully closes the SSH connection
+                dq.task_done() # Indicate that a formerly enqueued task is complete
+                return
+            nc.disconnect() # Gracefully closes the SSH connection
+            logger(f'{thread_name} {ip}: Done!')
+            dq.task_done() # Indicate that a formerly enqueued task is complete
+        except BaseException as e:
+            log_failed(f'{thread_name} {ip}:\n {e}')
+            dq.task_done() # Indicate that a formerly enqueued task is complete
+
+if args.w == 'report.log': # Creates a unique report name
+    args.w = f'{datetime.now().strftime("%m-%d-%Y_%H-%M-%S")}-{args.i[0]}-report.log' # Standard log file to log all events
+    failedfile = f'{datetime.now().strftime("%m-%d-%Y_%H-%M-%S")}-{args.i[0]}-failed-report.log' # If there are any issues, output to a failed file
+    sumfile = f'{datetime.now().strftime("%m-%d-%Y_%H-%M-%S")}-{args.i[0]}-summary-report.log' # Output the success to separate file
+else:
+    failedfile = f'{args.w}-failed-report.log'
+    sumfile = f'{args.w}-summary-report.log'
+
+logger = log_file(args.w) # Set standard log file
+log_failed = log_file(failedfile) # Set log file if there is a failed device
+out_sum = log_sum(sumfile) # Set log file for summary output
+
+def main():
+    if args.k: # Prompts for SSH password
+        password = getpass.getpass(f'{args.u} password:')
+    
+    log_folder()
+    daily_runtime_folder()
+
+    ip_list = [addr.replace('.', '\.') for addr in args.i] # Converts each IP in the list to escape the dot between the octects
+
+    logger('Starting search...')
+    out_sum('Starting search...')
+    start_time = datetime.now()
+
+    device_queue = Queue(maxsize=0) # Opens a queue for the device network connection information
+    device_details = Queue(maxsize=0) # Opens a queue for the detailed device information
+
+    x = 0 # Variable to increment JSON index in below for loop
+
+    try:
+        r = requests.get(api_url, auth = (args.u, password), params = http_params, verify = not args.n) # API call to XMC server
+        if r.status_code != requests.codes.ok:
+            r.raise_for_status()
+        result = r.json() # Convert result to JSON format
+        for device in result['data']['network']['devices']:
+            new_device = NET_DEVICE.copy()
+            if result['data']['network']['devices'][x]['nickName'] is None:
+                x += 1
+                continue
+            router_check = grab_routers(result['data']['network']['devices'][x]['nickName'])
+            if router_check == False:
+                x += 1
+                continue
+            vendor = discover_vendor(result['data']['network']['devices'][x]['nickName'], result['data']['network']['devices'][x]['nosIdName'])
+            if vendor == 'null':
+                x += 1
+                continue
+            new_device['device_type'] = vendor # Pulls vendor from hostname, which is used as the connection profile for Netmiko
+            new_device['ip'] = result['data']['network']['devices'][x]['ip'] # Pulls IP to connect to
+            new_device['password'] = password # Puts the prompted SSH user password into the dictionary
+            device_queue.put(new_device) # Loads device network connetion information into the queue
+            device_details.put(device)
+            x += 1
+    except BaseException as e:
+        log_failed(f'Failed to fetch {device} data from XMC:')
+        log_failed(e)
+        exit(255)
+        
+    total_devices = device_queue.qsize() # Used in the max thread calculation
+    logger(f'\nSearching {total_devices} routers')
+    out_sum(f'\nSearching {total_devices} routers')
+
+    run_threads(total_devices=total_devices, mt_function=send_commands, dq=device_queue, dd=device_details, ip_list=ip_list) # Calls the multithreading function
+    device_queue.join() # Blocks until all items in the queue have been gotten and processed
+
+    logger(f'\nElapsed time: {str(datetime.now() - start_time)}') # Total runtime for the script
+    out_sum(f'\nElapsed time: {str(datetime.now() - start_time)}') # Total runtime for the script
+
+if __name__ == "__main__": # Check if script is called by the user or imported
+    main()
