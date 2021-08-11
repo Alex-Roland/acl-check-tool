42fc39f4c371c69e7f6c2e73011b83838c9c82e1
Alex-Roland <47874780+Alex-Roland@users.noreply.github.com>
2021-08-11 15:18:47 -0500

    Update act.py

--- act.py
+++ act.py
@@ -48,8 +48,8 @@ parser.add_argument('-V', help = 'display version information', default = False,
 args = parser.parse_args()
 
 NAME = 'ACL Check Tool'
-VERSION = '2.2.1'
-DATE = '08/09/2021'
+VERSION = '2.2.2'
+DATE = '08/11/2021'
 
 if args.V == True: # Script version information
     print('|' + '-'*30 + '|')
